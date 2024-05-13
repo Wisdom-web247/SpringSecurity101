3ee9b87dc9bb771d44f70d1260978fb7841d2d86
Wisdom-web247 <wizzymahlangu@gmail.com>
2024-05-14 00:29:25 +0200

    Updated the @Springboot application file

--- src/main/java/com/example/springsecurity101/SpringSecurity101Application.java
+++ src/main/java/com/example/springsecurity101/SpringSecurity101Application.java
@@ -3,11 +3,14 @@ package com.example.springsecurity101;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class SpringSecurity101Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurity101Application.class, args);
+        System.out.println("Server running on port: 8080");
     }
 
 }
